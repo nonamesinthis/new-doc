6b759b0633d652e91213ce15d158ea3e90bb870d
nonamesinthis <dmuchowiecki@gmail.com>
2024-03-21 21:52:58 +0100

    x213

--- newDoc.py
+++ newDoc.py
@@ -5,6 +5,8 @@ client = MongoClient("mongodb://localhost:28080")
 
 db = client["new"]
 collection = db['main']
-collection.insert_one({"data": "xd"})
-result = collection.find_one({'data': 'xd'})
+collection.insert_one({"username": "hege","password":"123"})
+collection.insert_one({"username": "k","password":"m"})
+result = collection.find_one({})
 print("aha o to wynik: ",result)
+
